a15d870421399e366021dadb99d9a648ab619ce1
Aaron Watson <52084480+rikudou7@users.noreply.github.com>
2022-08-10 11:23:53 -0400

    Updated preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,17 +1,31 @@
 const { ipcRenderer, contextBridge, desktopCapturer } = require("electron");
+const electron = require("electron");
+const dialog = electron.remote.dialog;
+const path = require("path");
+const fs = require("fs");
+
+const Buffer = require('buffer/').Buffer;
+//console.log(Buffer.from("41", 'hex')[0]); returns 65
+
+
+
 
 
 // Expose protected methods off of window (ie.
-// window.api.require) in order to use ipcRenderer
+// window.api.fs()) in order to use ipcRenderer
 // without exposing the entire object
 contextBridge.exposeInMainWorld("api", {
-    getScreen: async function () {const screen = await desktopCapturer.getSources({ types: ['screen'] }); return screen;}
+    getScreen: async function () { const screen = await desktopCapturer.getSources({ types: ['screen'] }); return screen; },
+    dialog: () => dialog,
+    path: () => path,
+    fs: () => fs,
+    buffer: (data) => Buffer.from(data, 'base64')
 });
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping'),
+    ping: () => ipcRenderer.invoke('ping')
     // we can also expose variables, not just functions
 })
